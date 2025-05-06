ab90e31c901d207b83acb76de48a4d9d886ef1e2
revyera188 <jenny123.mueller@gmail.com>
2025-05-06 15:42:02 +0200

    sum changes
    finished tutorial 04

--- Player/Scripts/player.gd
+++ Player/Scripts/player.gd
@@ -15,8 +15,10 @@ func _ready():
 
 
 func _process(delta):
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	direction = Vector2(
+		Input.get_axis("left", "right"),
+		Input.get_axis("up", "down")
+	).normalized()
 	pass
 
 
